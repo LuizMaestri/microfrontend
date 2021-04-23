9a058bdc566ae6687fb8b3a58216a71dca9ed6fa
Luiz Maestri <luizricardomaestri@gmail.com>
2021-04-23 00:34:12 -0300

    vue todo list app parcel

---
+++ packages/todo-list/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <form @submit.prevent="add()">
+      <input type="text" v-model="task">
+      <button type="submit">Add</button>
+    </form>
+    <ul>
+      <li v-for="t in tasks" :key="t">{{t}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      task: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    add() {
+      if (this.task.trim()) {
+        this.tasks.push(this.task);
+        this.task = "";
+      }
+    },
+  },
+};
+</script>
